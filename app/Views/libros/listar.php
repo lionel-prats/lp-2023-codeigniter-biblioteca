1461ed0a970a3b6fb34ad20bbc8216e0015e8700
lionel-prats <lionel.prats.c@gmail.com>
2023-04-16 20:41:13 -0300

    BLOQUE 3 -> 31:08 (10)Instalación de bootstrap y errores

--- app/Views/libros/listar.php
+++ app/Views/libros/listar.php
@@ -5,8 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    Lista de libros
+    <div class="container bg-success">
+        <table class="table table-light">
+            <thead class="thead-light">
+                <tr>
+                    <th>#</th>
+                    <th>Imagen</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Imagen</td>
+                    <td>Nombre</td>
+                    <td>Editar/Borrar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
